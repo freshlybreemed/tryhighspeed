d421e2d271138f0997ae9ec29d932417305eaa52
Ebrima <ebrima.jobe92@gmail.com>
2020-11-03 20:23:51 -0800

    add title + price to product template

--- src/templates/product.tsx
+++ src/templates/product.tsx
@@ -1,13 +1,17 @@
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { formatPrice } from "../lib"
 
 import React from "react"
 
-const ProductPage = data => {
-  console.log(data)
+const ProductPage = ({ pageContext }) => {
+  console.log(pageContext)
   return (
     <Layout>
-      <SEO title={data.pageContext.title} />
+      <SEO title={pageContext.title} />
+      <h1>
+        {pageContext.title} {formatPrice(pageContext.price)}
+      </h1>
     </Layout>
   )
 }
