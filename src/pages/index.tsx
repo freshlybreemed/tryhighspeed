18cea5e033e6f5220b4eee04df15e6ef13082751
ebrima <ebrima.jobe92@gmail.com>
2020-11-08 14:43:37 -0500

    home update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -42,14 +42,20 @@ const IndexPage = () => {
         className="w-100 db center"
         src="https://tryhighspeed.com/wp-content/uploads/2019/05/tabletophighspeed.jpg"
       />
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-        <Image />
+      <div className="mw7 center tc">
+        <h3 className="">As Featured In</h3>
+        <div className="dt-ns">
+          <div className="dtc-l w-100-ns tc">YURR</div>
+          <div className="dtc-l w-100-ns tc">YAA</div>
+        </div>
+        <p>Welcome to your new Gatsby site.</p>
+        <p>Now go build something great.</p>
+        <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+          <Image />
+        </div>
+        <Link to="/page-2/">Go to page 2</Link> <br />
+        <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
       </div>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
     </Layout>
   )
 }
