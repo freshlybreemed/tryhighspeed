762c326e9199e5b8d54476ed578d9a681f05278d
ebrima <ebrima.jobe92@gmail.com>
2020-11-10 17:47:29 -0500

    center cart

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -38,14 +38,15 @@ const IndexPage = () => {
   return (
     <Layout>
       <SEO title="Home" />
-      <h1>My Shopping Cart</h1>
-      <p>Your shopping cart is empty</p>
-      <p>Now go build something great.</p>
+      <div className="tc">
+        <h2 className="pv4 mt3">My Shopping Cart</h2>
+        <p className="pv3">Your shopping cart is empty</p>
+      </div>
       <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
         <Image />
       </div>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+      {/* <Link to="/page-2/">Go to page 2</Link> <br />
+      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> */}
     </Layout>
   )
 }
