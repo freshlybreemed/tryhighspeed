7ae312a6aa7e1c28c47f13eceff24eaa38fbed3a
ebrima <ebrima.jobe92@gmail.com>
2020-11-12 17:55:10 -0500

    ssr import google api js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,28 @@
  */
 
 // You can delete this file if you're not using it
+// Import React so that you can use JSX in HeadComponents
+const React = require("react")
+
+const HtmlAttributes = {
+  lang: "en",
+}
+
+const HeadComponents = [
+  <script
+    src={`https://maps.googleapis.com/maps/api/js?key=${process.env.GATSBY_GOOGLE_API_KEY}&libraries=places`}
+  />,
+]
+
+const BodyAttributes = {
+  "data-theme": "dark",
+}
+
+exports.onRenderBody = (
+  { setHeadComponents, setHtmlAttributes, setBodyAttributes },
+  pluginOptions
+) => {
+  setHtmlAttributes(HtmlAttributes)
+  setHeadComponents(HeadComponents)
+  setBodyAttributes(BodyAttributes)
+}
