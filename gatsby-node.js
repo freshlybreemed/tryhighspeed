fcbe9e72ec0b5e76a3523cb1837a11bba6148273
ebrima <ebrima.jobe92@gmail.com>
2021-02-27 08:48:58 -0800

    api route

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,14 @@
-const path = require(`path`)
+const path = require(`path`);
+// const pkg = require("@woocommerce/woocommerce-rest-api");
+
+// const WooCommerceRestApi = pkg.default;
+
+// const WooCommerce = new WooCommerceRestApi({
+//   url: "https://tryhighspeed.com",
+//   consumerKey: process.env.GATSBY_WOO_KEY,
+//   consumerSecret: process.env.GATSBY_WOO_SECRET,
+//   version: "wc/v3",
+// });
 
 /**
  * Implement Gatsby's Node APIs in this file.
@@ -8,13 +18,27 @@ const path = require(`path`)
 
 // Log out information after a build is done
 exports.onPostBuild = ({ reporter }) => {
-  reporter.info(`Your Gatsby site has been built!`)
-}
+  reporter.info(`Your Gatsby site has been built!`);
+};
+
+exports.onCreateDevServer = ({ app }) => {
+  app.get("/hello", function (req, res) {
+    WooCommerce.get("products")
+      .then((response) => {
+        console.log(response);
+        res.send(response.data);
+      })
+      .catch((error) => {
+        console.log("dude", error);
+      });
+    // res.send("hello world");
+  });
+};
 
 // Create product pages dynamically
 exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions
-  const productTemplate = path.resolve(`src/templates/product.tsx`)
+  const { createPage } = actions;
+  const productTemplate = path.resolve(`src/templates/product.tsx`);
 
   const result = await graphql(`
     query {
@@ -29,9 +53,9 @@ exports.createPages = async ({ graphql, actions }) => {
         }
       }
     }
-  `)
-  result.data.allWcProducts.edges.forEach(edge => {
-    const { node } = edge
+  `);
+  result.data.allWcProducts.edges.forEach((edge) => {
+    const { node } = edge;
     createPage({
       path: `products/${edge.node.wordpress_id}`,
       component: productTemplate,
@@ -41,6 +65,6 @@ exports.createPages = async ({ graphql, actions }) => {
         price: node.price,
         image: node.categories,
       },
-    })
-  })
-}
+    });
+  });
+};
