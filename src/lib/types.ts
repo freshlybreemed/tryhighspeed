b199f95191f41b577017b88f9fddc2687c8f3951
ebrima <ebrima.jobe92@gmail.com>
2021-03-07 14:18:04 -0800

    ts type additions

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,31 +1,29 @@
 export type File = {
   node: {
-    id: string
-    name: string
-    relativeDirectory: string
+    id: string;
+    name: string;
+    relativeDirectory: string;
     childImageSharp: {
-      id: string
-      fixed: any
-    }
-  }
-}
+      id: string;
+      fixed: any;
+    };
+  };
+};
 export type WooProduct = {
-  node: {
-    id: string
-    wordpress_id: string
-    name: string
-    price: string
-    description: string
-    images: [
-      localFile: {
-        childImageSharp: {
-          fluid: any
-          fixed: any
-        }
-      }
-    ]
-    categories: {
-      wordpress_id: string
+  id: string;
+  wordpress_id: string;
+  name: string;
+  price: string;
+  description: string;
+  images: [
+    localFile: {
+      childImageSharp: {
+        fluid: any;
+        fixed: any;
+      };
     }
-  }
-}
+  ];
+  categories: {
+    wordpress_id: string;
+  };
+};
