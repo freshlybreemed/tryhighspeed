99ba8c5b614c4c17d9c7f97f8c75dca8fa466e1b
ebrima <ebrima.jobe92@gmail.com>
2021-03-07 14:18:14 -0800

    order lambda update

--- api/order.ts
+++ api/order.ts
@@ -1,4 +1,5 @@
 import { NowRequest, NowResponse } from "@vercel/node";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const pkg = require("@woocommerce/woocommerce-rest-api");
 
 const WooCommerceRestApi = pkg.default;
@@ -10,9 +11,58 @@ const WooCommerce = new WooCommerceRestApi({
   version: "wc/v3",
 });
 
+const data = {
+  payment_method: "custom_payment",
+  payment_method_title: "Connexus Secure Payment",
+  set_paid: true,
+  billing: {
+    first_name: "John",
+    last_name: "Doe",
+    address_1: "969 Market",
+    address_2: "",
+    city: "San Francisco",
+    state: "CA",
+    postcode: "94103",
+    country: "US",
+    email: "john.doe@example.com",
+    phone: "(555) 555-5555",
+  },
+  shipping: {
+    first_name: "John",
+    last_name: "Doe",
+    address_1: "969 Market",
+    address_2: "",
+    city: "San Francisco",
+    state: "CA",
+    postcode: "94103",
+    country: "US",
+  },
+  line_items: [
+    {
+      product_id: 4810,
+      variation_id: 4814,
+      quantity: 1,
+    },
+  ],
+  shipping_lines: [
+    {
+      method_id: "flat_rate",
+      method_title: "Flat Rate",
+      total: "10.00",
+    },
+  ],
+};
+
+// WooCommerce.post("orders", data)
+//   .then((response) => {
+//     console.log(response.data);
+//   })
+//   .catch((error) => {
+//     console.log(error.response.data);
+//   });
 export default (request: NowRequest, response: NowResponse) => {
   const { name = "World" } = request.query;
-  WooCommerce.get("products")
+  WooCommerce.get("orders")
     .then((res: any) => {
       // console.log(response);
       response.send(res.data);
