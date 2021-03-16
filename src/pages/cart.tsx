8fabce379f4ce0562d1ac84c7f62a8d37061a5c2
Ebrima <ebrima.jobe92@gmail.com>
2021-03-16 03:22:42 -0700

    shopping cart getting there

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { graphql, Link, useStaticQuery } from "gatsby";
 
 import Layout from "../components/layout";
-import Image from "../components/image";
+import Img from "gatsby-image";
 import SEO from "../components/seo";
 import { useCartContainer } from "../components/cartContainer";
 import { WooProduct } from "../lib/types";
@@ -44,6 +44,10 @@ const CartPage = () => {
             product_variations {
               id
               price
+              attributes {
+                name
+                option
+              }
             }
           }
         }
@@ -56,12 +60,18 @@ const CartPage = () => {
     const item = edges.filter(
       (prod) => prod.node.wordpress_id === line.product_id
     )[0].node;
-    const price = item.product_variations.filter(
+    const variant = item.product_variations.filter(
       (vari) => vari.id === line.variation_id
-    )[0].price;
+    )[0];
+    const speed = variant.attributes.filter((attr) => attr.name === "Speed")[0];
+    variant.attributes = variant.attributes.filter(
+      (attr) => attr.name !== "Speed"
+    );
     return {
       ...line,
-      price,
+      price: variant.price,
+      variant,
+      speed,
       ...item,
     };
   });
@@ -72,32 +82,57 @@ const CartPage = () => {
       <div className="pl2 ml2 ">
         <SEO title="Home" />
         <div className="pv3 mv3 ">
-          <h1 className="fw3 f1">Your Shopping Cart</h1>
+          <h1 className="fw3 mb-3 pb-3 text-3xl cubano text-center f1">
+            Your Shopping Cart
+          </h1>
         </div>
-        <table className="min-w-full ">
-          <thead>
-            <tr>
-              <th>Product</th>
-              <th>Amount</th>
-              <th>Speed</th>
-              <th>Total</th>
-            </tr>
-          </thead>
-          <tbody>
-            {items.map((product) => {
-              return (
-                <tr>
-                  <td>{product.name}</td>
-                  <td>{product.quantity}</td>
-                  <td>{product.product_id}</td>
-                  <td>{formatPrice(product.price)}</td>
+        <div className="w-11/12 rounded-lg bg-white m-auto p-4">
+          {lineItems.length ? (
+            <table className="min-w-full m-4">
+              <thead>
+                <tr className="text-left cubano">
+                  <th>Product</th>
+                  <th>Speed</th>
+                  <th>Total</th>
                 </tr>
-              );
-            })}
-          </tbody>
-        </table>
-        <p className="tc f3 fw3">Your shopping cart is empty</p>
-        <Link to="/checkout">Checkout</Link> <br />
+              </thead>
+              <tbody>
+                {items.map((product) => {
+                  return (
+                    <tr className="text-left my-2">
+                      <td>
+                        <div className="flex items-center">
+                          <Img
+                            className="sm:w-10 md:w-10 w-20"
+                            fluid={
+                              product.images[0].localFile.childImageSharp.fluid
+                            }
+                          ></Img>
+                          <div className="pl-2">
+                            <p className="text-sm">{product.name}</p>
+                            {product.variant.attributes.map((attr) => {
+                              return <p className="text-xs">{attr.option}</p>;
+                            })}
+                          </div>
+                        </div>
+                      </td>
+                      <td className="text-xs">{product.speed.option}</td>
+                      <td className="text-xs">{formatPrice(product.price)}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          ) : (
+            <p className="text-center text-lg gt">Cart is empty :(</p>
+          )}
+          <Link to="/checkout">
+            <button className="bg-black text-white py-2 px-4 rounded gt text-right">
+              Checkout
+            </button>
+          </Link>{" "}
+        </div>
+        <br />
       </div>
     </Layout>
   );
